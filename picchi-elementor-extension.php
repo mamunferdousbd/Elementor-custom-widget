8c462f38f349cc2783c9db6768e5e5805e1c1c05
abdullah_mamun <aamun22@gmail.com>
2022-07-03 10:24:43 -0700

    feature widget added and linked

--- picchi-elementor-extension.php
+++ picchi-elementor-extension.php
@@ -249,6 +249,7 @@ final class Elementor_Picchi_Extension {
 		require_once( __DIR__ . '/widgets/banner-widget.php' );
 		require_once( __DIR__ . '/widgets/heading-widget.php' );
 		require_once( __DIR__ . '/widgets/about-widget.php' );
+		require_once( __DIR__ . '/widgets/feature-widget.php' );
 
 		// added by EWA - EWA own Register widgets, loading all widget names
 
@@ -256,6 +257,7 @@ final class Elementor_Picchi_Extension {
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Banner_Widget() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Heading_widget() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \About_widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Feature_widget() );
 
 	}
 
---
+++ widgets/feature-widget.php
@@ -0,0 +1,323 @@
+<?php
+class Feature_widget extends \Elementor\Widget_Base{
+
+    // Widget Name
+    public function get_name() {
+        return 'Feature Widget';
+    }
+    // Widget Title
+	public function get_title() {
+        return esc_html( 'Feature Widget','picchi-extension' );
+    }
+    // Widget Icon
+	public function get_icon() {
+        return 'eicon-plus';
+    }
+    // Help for Widget
+	public function get_custom_help_url() {
+        return 'https://www.mamunferdousbd.com';
+    }
+    // Widget Categories Basic,General,Pro Etc.
+	public function get_categories() {
+        return ['general'];
+    }
+    // For Searching Widget 
+	public function get_keywords() {
+        return ['featured','feature','widget','pagefeature'];
+    }
+    
+    protected function register_controls(){
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label'=>esc_html__( 'Content','picchi-extension' ),
+                'tab'=> \Elementor\Controls_Manager::TAB_CONTENT,
+
+            ]
+        );
+
+        // Sub Heading
+        $this->add_control(
+            'section_title_sub_heading',
+            [
+                'label'=>esc_html__( 'Sub Heading','picchi-extension' ),
+                'type'=>  \Elementor\Controls_Manager::TEXT,
+                'placeholder'=>esc_html__('Write Your Sub Heading Here.','picchi-extension'),
+                'seperator'=>'after',
+                'label_block'=>true,
+            ]
+        );
+        // Heading
+        $this->add_control(
+            'section_title_heading',
+            [
+                'label'=>esc_html__( 'Heading','picchi-extension' ),
+                'type'=>  \Elementor\Controls_Manager::TEXT,
+                'placeholder'=>esc_html__('Write Your Heading Here.','picchi-extension'),
+                'seperator'=>'after',
+                'label_block'=>true,
+            ]
+        );
+        // Description
+        $this->add_control(
+            'section_title_description',
+            [
+                'label'=>esc_html__( 'Description','picchi-extension' ),
+                'type'=>  \Elementor\Controls_Manager::TEXTAREA,
+                'dynamic' => [
+					'active' => true,
+				],
+                'rows'=>10,
+                'placeholder'=>esc_html__('Write Your Description Here.','picchi-extension'),
+                'seperator'=>'after',
+                'label_block'=>true,
+            ]
+        );
+        $this->add_control(
+			'section_title_align',
+			[
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'label' => esc_html__( 'Alignment', 'picchi-extension' ),
+				'options' => [
+					'left' => [
+						'title' => esc_html__( 'Left', 'picchi-extension' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'picchi-extension' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'picchi-extension' ),
+						'icon' => 'eicon-text-align-right',
+					],
+                    'justify' => [
+						'title' => esc_html__( 'Justified', 'elementor' ),
+						'icon' => 'eicon-text-align-justify',
+					],
+				],
+				'default' => 'center',
+				'selectors' => [
+					'{{WRAPPER}} .section-title' => 'text-align: {{VALUE}};',
+				],
+			]
+		);
+        
+        $this->end_controls_section();
+
+        /*===================
+            Style Tab
+        ===================== */
+        $this->start_controls_section(
+            'style_section',
+            [
+                'label'=>esc_html__( 'Style','picchi-extension' ),
+                'tab'=> \Elementor\Controls_Manager::TAB_STYLE,
+
+            ]
+        );
+        // Section Sub Title
+        $this->add_control(
+			'heading_sub_title',
+			[
+				'label' => esc_html__( 'Sub Title', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+        
+        // Sub Heading Color
+        $this->add_control(
+			'heading_sub_title_color',
+			[
+				'label' => esc_html__( 'Color', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+                'default'=>'#fff',
+				'selectors' => [
+					'{{WRAPPER}} .section-title h4' => 'color: {{VALUE}}',
+				],
+			]
+		);
+        // Sub Heading typography
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'heading_sub_title_typography',
+				'selector' => '{{WRAPPER}} .section-title h4',
+			]
+		);
+        // Section Heading Title
+        $this->add_control(
+			'heading_title',
+			[
+				'label' => esc_html__( 'Heading', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+        
+        // Heading Color
+        $this->add_control(
+			'heading_title_color',
+			[
+				'label' => esc_html__( 'Color', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+                'default'=>'#fff',
+				'selectors' => [
+					'{{WRAPPER}} .section-title h2' => 'color: {{VALUE}}',
+				],
+			]
+		);
+        //  Heading Typography
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'heading_title_typography',
+				'selector' => '{{WRAPPER}} .section-title h2',
+			]
+		);
+        // Border 
+        $this->add_control(
+			'heading_border_style',
+			[
+				'label' => esc_html__( 'Border', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+        // border One Background Color
+        $this->add_control(
+			'heading_border1_color',
+			[
+				'label' => esc_html__( 'Color', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+                'default'=>'#777',
+				'selectors' => [
+					'{{WRAPPER}} .section-title h2::before' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+        // border Two Background Color
+        $this->add_control(
+			'heading_border2_color',
+			[
+				'label' => esc_html__( 'Color', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+                'default'=>'#e16038',
+				'selectors' => [
+					'{{WRAPPER}} .section-title h2::after' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+
+        // Description
+        $this->add_control(
+			'heading_des_style',
+			[
+				'label' => esc_html__( 'Description', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+
+        // Description Color
+        $this->add_control(
+			'section_des_color',
+			[
+				'label' => esc_html__( 'Color', 'picchi-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+                'default'=>'#FFF',
+				'selectors' => [
+					'{{WRAPPER}} .section-title p' => 'color: {{VALUE}}',
+				],
+			]
+		);
+    
+        // Description Typography
+        
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'section-title_dsc_typography',
+				'selector' => '{{WRAPPER}} .section-title p',
+			]
+		);
+        
+
+
+        
+        
+
+
+        $this->end_controls_section();
+        
+
+        
+
+        
+    }
+
+    protected function render(){
+        $settings = $this->get_settings_for_display();
+        $section_title_sub_heading = $settings['section_title_sub_heading'];
+        $section_title_heading = $settings['section_title_heading'];
+        $section_title_description = $settings['section_title_description'];
+        
+
+        ?>
+    	<div class="features features-1">
+			
+		<div class="col-xl-6">
+			<div class="single-feature text-center">
+				<h6>Highly Creative Solutions</h6>
+				<h4>we are digital</h4>
+				<div class="row">
+					<div class="col-xl-6">
+						<div class="feature-box">
+							<i class="far fa-newspaper"></i>
+							<h5>modern design</h5>
+							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="feature-box">
+							<i class="far fa-newspaper"></i>
+							<h5>mobile firendly</h5>
+							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="feature-box">
+							<i class="far fa-newspaper"></i>
+							<h5>error free</h5>
+							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="feature-box">
+							<i class="far fa-newspaper"></i>
+							<h5>24/7 support</h5>
+							<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latinin contaversy. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+			
+	</div>
+
+
+        <?php
+
+
+    }
+    
+
+    
+
+	
+
+}
